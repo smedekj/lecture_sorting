77c03e5cd9c8541a3f4740d888500e50f4b4bbfa
smedekj <smedek.j@seznam.cz>
2025-04-14 13:07:01 +0200

    update_2

--- sorting.py
+++ sorting.py
@@ -23,9 +23,28 @@ def read_data(file_name):
     return data
 
 
+def selection_sort(number_array, direction='ascending'):
+    l = len(number_array)
+    for i in range(l):
+        min_max_idx = i
+        for num_idx in range(i+1, l):
+            if direction == 'ascending':
+                if number_array[num_idx] < number_array[min_max_idx]:
+                    min_max_idx = num_idx
+            elif direction == 'descending':
+                if number_array[num_idx] > number_array[min_max_idx]:
+                    min_max_idx = num_idx
+
+        number_array[i], number_array[min_max_idx] = number_array[min_max_idx], number_array[i]
+
+    return number_array
+
+
 def main():
     data = read_data('numbers.csv')
     print(data)
+    sorted_nums = selection_sort(data['series_1'])
+    print(sorted_nums)
     pass
 
 
